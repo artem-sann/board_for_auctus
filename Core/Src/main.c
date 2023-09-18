7ff707ad080b5b50e04cf99c27119d17143e33ee
Artem Sannikov <artemksannikov@gmail.com>
2023-09-18 14:10:17 +0300

    blink auctus

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -99,6 +99,8 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+      HAL_GPIO_TogglePin(Led_GPIO_Port, Led_Pin);
+      HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
