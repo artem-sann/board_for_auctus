c8a7a9cb57ca2c169df5433616ef73af00194237
Artem Sannikov <artemksannikov@gmail.com>
2023-11-13 00:54:57 +0300

    simple_setup_auctus function ready

--- Core/Src/auctus.c
+++ Core/Src/auctus.c
@@ -3,7 +3,7 @@
 //
 #include "auctus.h"
 #include <string.h>
-#include <stdlib.h>
+#include "stdio.h"
 
 /**
  * @Note Функция базовой настройки модуля
@@ -16,8 +16,23 @@
  */
 uint8_t simple_setup_auctus(UART_HandleTypeDef *huart, uint8_t  volume, uint32_t addr, uint8_t dmo_channel, uint32_t freq)
 {
-    uint8_t *response = malloc(60);
+    uint8_t response[60];
     uint8_t state;
+    uint8_t STR_2[60];
+    uint8_t STR_3[60];
+    uint8_t STR_4[60];
+    uint8_t STR_5[60];
+    uint8_t STR_6[60];
+    uint8_t STR_7[60];
+
+
+    snprintf(STR_2, sizeof STR_2, "%s%d\n", STR_SWITCH, dmo_channel);
+    snprintf(STR_3, sizeof STR_3, "%s2,0,%lu,%lu,00,00,3,2\n", STR_GROUP, freq, freq);
+    snprintf(STR_4, sizeof STR_4, "%s", STR_CURRCH);
+    snprintf(STR_5, sizeof STR_5, "%s%d\n", STR_VOLUME, volume);
+    snprintf(STR_6, sizeof STR_6, "%s%d\n", STR_MIC, 6);
+    snprintf(STR_7, sizeof STR_7, "%s%lu\n", STR_GRPADDR, addr);
+
 
     while(HAL_UART_Transmit_IT(huart,STR_CONNECT, strlen(STR_CONNECT)) == HAL_BUSY );
     HAL_UART_Receive_IT(huart, response, 60);
@@ -26,9 +41,63 @@ uint8_t simple_setup_auctus(UART_HandleTypeDef *huart, uint8_t  volume, uint32_t
     while((state == HAL_UART_STATE_BUSY_RX) || (state == HAL_UART_STATE_BUSY_TX_RX)){
         state = HAL_UART_GetState(huart);
     }
+    if (response[12] != 48) return 1;
+    memset(response,0,sizeof(response));
 
 
+    while(HAL_UART_Transmit_IT(huart, STR_2, strlen(STR_2)) == HAL_BUSY );
+    HAL_UART_Receive_IT(huart, response, 60);
 
-    free(response);
+    state = HAL_UART_GetState(huart);
+    while((state == HAL_UART_STATE_BUSY_RX) || (state == HAL_UART_STATE_BUSY_TX_RX)){
+        state = HAL_UART_GetState(huart);
+    }
+    memset(response,0,sizeof(response));
+
+    while(HAL_UART_Transmit_IT(huart, STR_3, strlen(STR_3)) == HAL_BUSY );
+    HAL_UART_Receive_IT(huart, response, 60);
+
+    state = HAL_UART_GetState(huart);
+    while((state == HAL_UART_STATE_BUSY_RX) || (state == HAL_UART_STATE_BUSY_TX_RX)){
+        state = HAL_UART_GetState(huart);
+    }
+    memset(response,0,sizeof(response));
+
+    while(HAL_UART_Transmit_IT(huart, STR_4, strlen(STR_4)) == HAL_BUSY );
+    HAL_UART_Receive_IT(huart, response, 60);
+
+    state = HAL_UART_GetState(huart);
+    while((state == HAL_UART_STATE_BUSY_RX) || (state == HAL_UART_STATE_BUSY_TX_RX)){
+        state = HAL_UART_GetState(huart);
+    }
+    memset(response,0,sizeof(response));
+
+    while(HAL_UART_Transmit_IT(huart, STR_5, strlen(STR_5)) == HAL_BUSY );
+    HAL_UART_Receive_IT(huart, response, 60);
+
+    state = HAL_UART_GetState(huart);
+    while((state == HAL_UART_STATE_BUSY_RX) || (state == HAL_UART_STATE_BUSY_TX_RX)){
+        state = HAL_UART_GetState(huart);
+    }
+    memset(response,0,sizeof(response));
+
+    while(HAL_UART_Transmit_IT(huart, STR_6, strlen(STR_6)) == HAL_BUSY );
+    HAL_UART_Receive_IT(huart, response, 60);
+
+    state = HAL_UART_GetState(huart);
+    while((state == HAL_UART_STATE_BUSY_RX) || (state == HAL_UART_STATE_BUSY_TX_RX)){
+        state = HAL_UART_GetState(huart);
+    }
+    memset(response,0,sizeof(response));
+
+    while(HAL_UART_Transmit_IT(huart, STR_7, strlen(STR_7)) == HAL_BUSY );
+    HAL_UART_Receive_IT(huart, response, 60);
+
+    state = HAL_UART_GetState(huart);
+    while((state == HAL_UART_STATE_BUSY_RX) || (state == HAL_UART_STATE_BUSY_TX_RX)){
+        state = HAL_UART_GetState(huart);
+    }
+    memset(response,0,sizeof(response));
+
     return 0;
 }
